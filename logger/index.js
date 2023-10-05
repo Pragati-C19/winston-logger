8f974bf1167be04393a56af6389d3b647ae8eea0
Pragati Chothe <pragatichothe@gmail.com>
2023-10-05 21:02:59 +0530

    Nodemon is installed

--- logger/index.js
+++ logger/index.js
@@ -1,1 +1,10 @@
 let logger=null
+
+// sample code from winston document
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple(),
+    }));
+}
+
+module.exports = logger;
